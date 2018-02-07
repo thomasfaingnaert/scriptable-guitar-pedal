815887aac5dc6a003fa57bb79399794623a1dba7
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2018-02-07 13:04:32 +0100

    Rewrite delay implementation

--- effect-testing/delaytest.m
+++ effect-testing/delaytest.m
@@ -1,20 +1,7 @@
-A = load('../samples/audiotest'); % Hallelujah from Handel with 8192 Hz
-Fs = 8192;
-delay = 0.5; % In seconds
-decay = 0.6;
-lengthlim = 3; % In seconds
-
-y = getfield(A,'y');
+[y,Fs] = audioread('dry-guitar-cut.wav');
+delayT = 0.2; % In seconds
+decay = 0.8;
 
 
-% Only 1 echo
-%{
-buf = Delay.readSamples(y); % Makes sure that input is in right format
-output = Delay.makeOutputSamples1(buf, Fs, delay, decay);
-sound(output);
-%}
-
-% Multiple echoes
-buf = Delay.readSamples(y); % Makes sure that input is in right format
-output = Delay.makeOutputSamples2(buf, Fs, delay, decay, lengthlim);
-sound(output);
+output = delay.makeOutputSamples(y, Fs, delayT, decay);
+sound(output,Fs);
