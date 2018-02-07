39149a2429f3a4f0e3fe970b60794564e9c73d7a
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2018-02-07 15:15:53 +0100

    Add another implementation of distortion

--- effect-testing/distortion.m
+++ effect-testing/distortion.m
@@ -22,14 +22,36 @@ classdef distortion
             y=-3/8.* y .^3 + 9/8 .* y;
         end
         
-        function y = makeOutputSamples2(x,tau)
+        function y = makeOutputSamples2(x,tau, vol)
             %MAKEOUTPUTSAMPLES2 adds distortion to the samples
             %   x contains the samples
             %   tau contains the 'steepness' of the function that
+            %   vol regulates the volume
             %   calculates distortion
+            
+            % check if vol has valid value
+            if vol > 1 || vol <= 0
+                vol = 1;
+            end
 
             % sigmoid curve
-            y = sign(x) .* (1 - exp(-tau*abs(x)));
+            y = vol * sign(x) .* (1 - exp(-tau*abs(x)));
+        end
+        
+        % This function creates a milder distortion because it uses soft
+        % clipping instead of hard clipping.
+        function y = makeOutputSamples3(x, alpha, vol)
+            %MAKEOUTPUTSAMPLES3 adds distortion to the samples
+            %   x contains the samples
+            %   alpha affects the amount of clipping
+            %   vol regulates the volume
+            
+            if vol > 1 || vol <= 0
+                vol = 1;
+            end
+            
+            % soft clipping
+            y = vol * 2/pi * atan(alpha * x);
         end
         
     end
--- effect-testing/distortiontest.m
+++ effect-testing/distortiontest.m
@@ -1,9 +1,10 @@
 [y,Fs]=audioread('dry-guitar-cut.wav');
 
+%{
 % Formula 1
 treshold = 0.2;
 vol = 0.1; 
-output = distortion.makeOutputSamples1(y,treshold, vol);
+output = distortion.makeOutputSamples1(y,treshold,vol);
 
 sound(output,Fs);
 audiowrite('distortion1.wav',output,Fs);
@@ -11,16 +12,30 @@ audiowrite('distortion1.wav',output,Fs);
 plot(output(1:500), 'r');
 hold on;
 plot(y(1:500), 'b');
+%}
 
 %{
 % Formula 2
-tau = 10;
-output = distortion.makeOutputSamples2(y,tau);
+tau = 30;
+vol = 0.5;
+output = distortion.makeOutputSamples2(y,tau,vol);
 
-%sound(output,Fs);
-audiowrite('distortion1.wav',output,Fs);
+sound(output,Fs);
+audiowrite('distortion2.wav',output,Fs);
 
 plot(output(1:500), 'r');
 hold on;
 plot(y(1:500), 'b');
 %}
+
+% Formula 3
+alpha = 15;
+vol = 0.8;
+output = distortion.makeOutputSamples3(y,alpha,vol);
+
+sound(output,Fs);
+audiowrite('distortion3.wav',output,Fs);
+
+plot(output(1:500),'r');
+hold on;
+plot(y(1:500),'b');
