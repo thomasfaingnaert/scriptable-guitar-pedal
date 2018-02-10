2a3eb6f129e44408f31b6711e7409cf5b93dfa4d
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-02-10 14:15:11 +0100

    Update agc test sample

--- effect-testing/agctest.m
+++ effect-testing/agctest.m
@@ -1,8 +1,8 @@
-[y,Fs] = audioread('../samples/smoke-on-the-water.wav');
+[y,Fs] = audioread('../samples/agc-test.wav');
 
-input_power_treshold = -55;
+input_power_treshold = -33;
 output_power = -22;
-block_size = 0.5 * Fs;
+block_size = 0.75 * Fs;
 
 output = agc.makeOutputSamples(y, input_power_treshold, output_power, block_size);
 sound(output, Fs);
