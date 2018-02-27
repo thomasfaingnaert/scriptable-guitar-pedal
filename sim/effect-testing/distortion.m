d99a7ed14a24ecfb1519984cac496f1a31882545
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2018-02-27 10:16:00 +0100

    Change asymmetric distortion implementation

--- sim/effect-testing/distortion.m
+++ sim/effect-testing/distortion.m
@@ -71,22 +71,32 @@ classdef distortion
         ways.
         %}
         
-        function y = makeOutputSamples4(x,alpha,vol)
+        function y = makeOutputSamples4(x, treshold,vol)
             %MAKEOUTPUTSAMPLES4 will add distortion to the samples
             %   x contains the samples
             %   treshold determines at which point the clipping starts
             %   vol determines outputvolume
             
             % eliminate noise
-            noisegate = 2 * 10^(-3);
+            noisegate = 5 * 10^(-4);
+            x = x .* (abs(x) >= noisegate);
             
+            % check if vol has valid value
             if vol > 1 || vol <= 0
                 vol = 1;
             end
+
+            % pre-gain
+            y = x./treshold;
+
+            % hard clipping to [-1,1]
+            y=min(1, max(y, -1));
             
-            % asymmetric clipping
-            y = vol * ((x.*(x >= noisegate)).^(1/alpha) + (x.*(x <= noisegate)));
-            y = y.* (abs(y) >= noisegate);
+            % Asymmetric distortion
+            y = y .* (y >= 0) + x .* (y < 0);
+
+            % smoothing
+            y=vol.*(-3/8.* y .^3 + 9/8 .* y);
         end
         
         function y = makeOutputSamples5(x,tau,vol)
@@ -107,6 +117,8 @@ classdef distortion
             
             % sigmoid curve
             y = vol * sign(x) .* (1 - exp(-tau*abs(x)));
+            
+            % asymmetric distortion
             y = y .* (y >= 0) + x .* (y < 0);
         end
         
@@ -126,6 +138,8 @@ classdef distortion
 
             % soft clipping
             y = vol * 2/pi * atan(alpha * x);
+            
+            % asymmetric distortion
             y = y .* (y >= 0) + x .* (y < 0);
         end
     end
