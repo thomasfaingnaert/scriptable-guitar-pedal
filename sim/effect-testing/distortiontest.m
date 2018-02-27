b3c4763cf7ab304f77ea164bb8cf127be732d615
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2018-02-27 10:20:24 +0100

    Revise distortiontest script

--- sim/effect-testing/distortiontest.m
+++ sim/effect-testing/distortiontest.m
@@ -1,7 +1,7 @@
 [y,Fs]=audioread('../samples/smoke-on-the-water.wav');
 
 if exist('formula', 'var') ~= 1
-    disp('Please create a variable ''formula'' in the current workspace, set it to 1, 2 or 3 and run this script again.');
+    disp('Please create a variable ''formula'' in the current workspace, set it to a number between 1 and 6 and run this script again.');
     return;
 end
 
@@ -27,8 +27,29 @@ switch formula
         output = distortion.makeOutputSamples3(y,alpha,vol);
 
         sound(output,Fs);
+    case 4
+        % Formula 4
+        treshold = 0.1;
+        vol = 0.8;
+        output = distortion.makeOutputSamples4(y,treshold,vol);
+
+        sound(output,Fs);
+    case 5
+        % Formula 5
+        tau = 30;
+        vol = 0.8;
+        output = distortion.makeOutputSamples5(y,tau,vol);
+
+        sound(output,Fs);
+    case 6 
+        % Formula 6
+        alpha = 25;
+        vol = 0.8;
+        output = distortion.makeOutputSamples3(y,alpha,vol);
+
+        sound(output,Fs);
     otherwise
-        disp('Formula should be 1, 2 or 3.');
+        disp('Formula should be between 1 and 6.');
         return;
 end
 
