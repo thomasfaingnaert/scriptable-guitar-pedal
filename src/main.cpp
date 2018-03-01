1622b913961b627fb12c11fd307bc81ac6788d26
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-03-01 22:52:50 +0100

    Clean up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,7 @@
-#include <algorithm>
-#include <array>
-#include <cstddef>
 #include <cstdlib>
 #include <iostream>
-#include <iterator>
-#include <numeric>
 
-#include "sndfile.hh"
 #include "sampledata.h"
-
-void write()
-{
-    std::cout << "Writing file...\n";
-    static constexpr size_t BUFFER_LENGTH = 1024;
-    std::array<double, BUFFER_LENGTH> buffer;
-
-    std::iota(buffer.begin(), buffer.end(), 0);
-    std::copy(buffer.begin(), buffer.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << "\n";
-
-    SndfileHandle file("test.wav", SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_24, 2, 48000);
-    file.write(buffer.data(), buffer.size());
-}
-
-void read()
-{
-    std::cout << "Reading file...\n";
-    static constexpr size_t BUFFER_LENGTH = 1024;
-    std::array<double, BUFFER_LENGTH> buffer;
-
-    SndfileHandle file("test.wav");
-
-    std::cout << "Sample rate: " << file.samplerate() << "\n"
-        "Channels: " << file.channels() << "\n";
-
-    file.read(buffer.data(), buffer.size());
-
-    std::copy(buffer.begin(), buffer.end(), std::ostream_iterator<double>(std::cout, " "));
-    std::cout << "\n";
-}
 
 int main(int argc, char *argv[])
 {
