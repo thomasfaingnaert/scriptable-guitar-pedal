7eb35a1f3b6cf89617de0aaed88d19a566017cfe
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-03-03 10:15:59 +0100

    Use FFT in overlap save algorithm

--- sim/algorithms/overlapsave.m
+++ sim/algorithms/overlapsave.m
@@ -1,8 +1,10 @@
-x = rand([1 3*29*1000]);
-h = rand([1 30]);
+%x = rand([1 3*29*1000]);
+%h = rand([1 30]);
+x = 1:36;
+h = [1 0 0 0 0];
 y = overlapSave(x, h);
 c = conv(x, h);
-if isequal(c(1:length(x)), y) 
+if all(abs(c(1:length(x)) - y) < 1e-12) 
    disp('Output OK'); 
 end
 
@@ -15,11 +17,17 @@ function y = overlapSave(x, h)
     % buffer which remembers previous samples
     buf = zeros([1 overlap]);
     
+    % period for FFT
+    N = 4 * overlap;
+    
     % process input per block_size samples
-    block_size = 3 * overlap;
+    block_size = N - overlap;
     
     % current output and input position
     pos = 0;
+    
+    % calculate frequency response
+    H = fft(h, N);
     
     % initialize output
     y = [];
@@ -29,7 +37,7 @@ function y = overlapSave(x, h)
        input_segment = [buf x(pos+1:pos+block_size)];
        
        % calculate corresponding output segment
-       output_segment = conv(input_segment, h);
+       output_segment = ifft(fft(input_segment, N) .* H, N);
        
        % append output
        y = [y output_segment(M:M+block_size-1)];
