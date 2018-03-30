6bbac71569915749b829ee08ab1eee8feeb63343
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2018-03-30 15:38:26 +0200

    Remove unnecessary files

--- public/assets/js/add-effect.js
+++ public/assets/js/add-effect.js
@@ -114,7 +114,7 @@ function addEffect() {
         effect = effect.charAt(0).toUpperCase() + effect.slice(1).toLowerCase();
 
         var box = '<div class="jsplumb-box" id="box-' + numElements + '"' +
-            'data-effect="' + formData + '" ' +
+            'data-effect=\'' + formData + '\' ' +
             'onclick="$(\'#changeEffectModal\').modal(\'show\'); changeEffect(this);">\n' +
             '               <p>' + effect + '</p>\n' +
             '            </div>';
