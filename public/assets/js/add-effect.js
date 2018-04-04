f9c1fdb9d971e9254d960dd700c6dc78604fcc08
Robbe Vanhaesebroeck <robbe.vanhaesebroeck@ugent.be>
2018-04-04 11:25:01 +0200

    Rework form

--- public/assets/js/add-effect.js
+++ public/assets/js/add-effect.js
@@ -23,75 +23,83 @@ $(document).ready(function () {
 function displayForm(effect) {
     var form = "";
     if (effect === 'distortion') {
-        form = "<p>\n" +
-            "                    <input type=\"radio\" name=\"type\" value=\"symmetric\"> Symmetric<br/>\n" +
-            "                    <input type=\"radio\" name=\"type\" value=\"asymmetric\"> Asymmetric<br/>\n" +
-            "                </p>\n" +
-            "                <p>\n" +
-            "                    Gain:<br/>\n" +
+        form = "<div class=\"form-group\">\n" +
+            "                    <div class=\"form-check\">\n" +
+            "                        <input type=\"radio\" class=\"form-check-input\" name=\"type\" id=\"type1\" value=\"symmetric\"/>\n" +
+            "                        <label class=\"form-check-label\"> Symmetric </label>\n" +
+            "                    </div>\n" +
+            "                    <div class=\"form-check\">\n" +
+            "                        <input type=\"radio\" class=\"form-check-input\" name=\"type\" id=\"type2\" value=\"asymmetric\"/>\n" +
+            "                        <label class=\"form-check-label\"> Asymmetric </label>\n" +
+            "                    </div>\n" +
+            "                </div>\n" +
+            "                <div class=\"form-group\">\n" +
+            "                    <label for=\"gain1\">Gain</label> <br/>\n" +
             "                    0\n" +
-            "                    <input type=\"range\" name=\"gain\" min=\"0\" max=\"10\" step=\"0.1\"/>\n" +
+            "                    <input type=\"range\" name=\"gain1\" id=\"gain1\" min=\"0\" max=\"10\" step=\"0.1\"/>\n" +
             "                    10\n" +
-            "                </p>\n" +
-            "                <p>\n" +
-            "                    Gain 2 (only Asymmetric):<br/>\n" +
+            "                </div>\n" +
+            "                <div class=\"form-group\">\n" +
+            "                    <label for=\"gain2\">Gain 2 (only Asymmetric)</label> <br/>\n" +
             "                    0\n" +
-            "                    <input type=\"range\" name=\"gain2\" min=\"0\" max=\"10\" step=\"0.1\"/>\n" +
+            "                    <input type=\"range\" name=\"gain2\" id=\"gain2\" min=\"0\" max=\"10\" step=\"0.1\"/>\n" +
             "                    10\n" +
-            "                </p>\n" +
-            "                <p>\n" +
-            "                    Mix:<br/>\n" +
+            "                </div>\n" +
+            "                <div class=\"form-group\">\n" +
+            "                    <label for=\"mix1\">Mix</label> <br/>\n" +
             "                    0\n" +
-            "                    <input type=\"range\" name=\"mix\" min=\"0\" max=\"1\" step=\"0.01\"/>\n" +
+            "                    <input type=\"range\" name=\"mix1\" id=\"mix1\" min=\"0\" max=\"1\" step=\"0.01\"/>\n" +
             "                    1\n" +
-            "                </p>\n" +
-            "                <p>\n" +
-            "                    Mix 2 (only Asymmetric):<br/>\n" +
+            "                </div>\n" +
+            "                <div class=\"form-group\">\n" +
+            "                    <label for=\"mix2\">Mix 2 (only Asymmetric</label> <br/>\n" +
             "                    0\n" +
-            "                    <input type=\"range\" name=\"mix2\" min=\"0\" max=\"1\" step=\"0.01\"/>\n" +
+            "                    <input type=\"range\" name=\"mix2\" id=\"mix2\" min=\"0\" max=\"1\" step=\"0.01\"/>\n" +
             "                    1\n" +
-            "                </p>\n" +
-            "                <p>\n" +
-            "                    Noise treshold:<br/>\n" +
+            "                </div>\n" +
+            "                <div class=\"form-group\">\n" +
+            "                    <label for=\"threshold\">Noise threshold</label> <br/>\n" +
             "                    0.0001\n" +
-            "                    <input type=\"range\" name=\"threshold\" min=\"0.0001\" max=\"0.01\" step=\"0.001\"/>\n" +
+            "                    <input type=\"range\" name=\"threshold\" id=\"threshold\" min=\"0.0001\" max=\"0.01\" step=\"0.001\"/>\n" +
             "                    0.01\n" +
-            "                </p>";
+            "                </div>";
     } else if (effect === 'delay') {
-        form = "<p>\n" +
-            "                    <input type=\"radio\" name=\"type\" value=\"fir\"/> One Echo<br/>\n" +
-            "                    <input type=\"radio\" name=\"type\" value=\"iir\"/> Multiple Echoes\n" +
-            "                </p>\n" +
-            "                <p>\n" +
-            "                    Delay Time:<br/>\n" +
-            "                    0.05s\n" +
-            "                    <input type=\"range\" name=\"delay\" min=\"0.05\" max=\"2\" step=\"any\"/>\n" +
-            "                    2s\n" +
-            "                </p>\n" +
-            "                <p>\n" +
-            "                    Decay:<br/>\n" +
-            "                    0.1\n" +
-            "                    <input type=\"range\" name=\"decay\" min=\"0.1\" max=\"0.9\" step=\"any\"/>\n" +
-            "                    0.9\n" +
-            "                </p>";
+        form = "<div class=\"form-group\">\n" +
+            "                    <div class=\"form-check\">\n" +
+            "                        <input type=\"radio\" class=\"form-check-input\" name=\"type\" id=\"type1\" value=\"fir\" />\n" +
+            "                        <label class=\"form-check-label\">One echo</label>\n" +
+            "                    </div>\n" +
+            "                    <div class=\"form-check\">\n" +
+            "                        <input type=\"radio\" class=\"form-check-input\" name=\"type\" id=\"type2\" value=\"iir\" />\n" +
+            "                        <label class=\"form-check-label\">Multiple echoes</label>\n" +
+            "                    </div>\n" +
+            "                </div>\n" +
+            "                <div class=\"form-group\">\n" +
+            "                    <label for=\"delay\">Delay time</label> <br/>\n" +
+            "                    0.05 <input type=\"range\" name=\"delay\" id=\"delay\" min=\"0.05\" max=\"2\" step=\"any\" /> 2\n" +
+            "                </div>\n" +
+            "                <div class=\"form-group\">\n" +
+            "                    <label for=\"decay\">Decay</label> <br/>\n" +
+            "                    0.1 <input type=\"range\" name=\"decay\" id=\"decay\" min=\"0.1\" max=\"0.9\" step=\"any\" /> 0.9\n" +
+            "                </div>";
     } else if (effect === 'tremolo') {
-        form = "<p>\n" +
-            "                    Depth:<br/>\n" +
+        form = "<div class=\"form-group\">\n" +
+            "                    <label for=\"depth\">Depth</label> <br/>\n" +
             "                    0.05\n" +
-            "                    <input type=\"range\" name=\"depth\" min=\"0.05\" max=\"0.95\" step=\"any\"/>\n" +
+            "                    <input type=\"range\" name=\"depth\" id=\"depth\" min=\"0.05\" max=\"0.95\" step=\"any\"/>\n" +
             "                    0.95\n" +
-            "                </p>\n" +
-            "                <p>\n" +
-            "                    Rate:<br/>\n" +
+            "                </div>\n" +
+            "                <div class=\"form-group\">\n" +
+            "                    <label for=\"rate\">Depth</label> <br/>\n" +
             "                    0.5 Hz\n" +
-            "                    <input type=\"range\" name=\"rate\" min=\"0.5\" max=\"10\" step=\"any\"/>\n" +
+            "                    <input type=\"range\" name=\"rate\" id=\"rate\" min=\"0.5\" max=\"10\" step=\"any\"/>\n" +
             "                    10 Hz\n" +
-            "                </p>";
+            "                </div>";
     } else if (effect === 'conv') {
-        form = " <p>\n" +
-            "                    Impulse Response:<br/>\n" +
-            "                    <input type=\"file\" name=\"impulse-response\" />\n" +
-            "                </p>";
+        form = "<div class=\"form-group\">\n" +
+            "                    <label for=\"impulse-response\">Input file</label>\n" +
+            "                    <input type=\"file\" class=\"form-control-file\" name=\"impulse-response\" id=\"impulse-response\" />\n" +
+            "                </div>";
     } else {
         alert("Select only 1 effect at a time please.");
     }
@@ -143,7 +151,7 @@ function parseFormData(form) {
     var unindexed = form.serializeArray();
     var jsonFormData = {};
 
-    $.map(unindexed, function(element, i) {
+    $.map(unindexed, function (element, i) {
         jsonFormData[element['name']] = element['value'];
     });
 
