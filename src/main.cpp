dbf82afaadd642fbcc6cb88f6b3c79fb369a15a4
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-11 11:43:52 +0200

    Remove unnecessary output from main

--- src/main.cpp
+++ src/main.cpp
@@ -37,21 +37,15 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    unsigned int IMPULSE_SIZE = 48000 * 0.01;
+    unsigned int IMPULSE_SIZE = 48000 * 0.1;
     unsigned int BLOCK_SIZE;
     std::cout << "Blocksize (samples): ";
     std::cin >> BLOCK_SIZE;
-    unsigned int inputSecs;
-    std::cout << "Input length (s): ";
-    std::cin >> inputSecs;
-    unsigned int NUM_BLOCKS = inputSecs * 48000 / BLOCK_SIZE;
+    unsigned int NUM_BLOCKS = 10 * 48000 / BLOCK_SIZE; // 10 seconds
 
     unsigned long long executionTime = timeDirectForm(IMPULSE_SIZE, BLOCK_SIZE, NUM_BLOCKS);
     unsigned long long inputTime = NUM_BLOCKS * BLOCK_SIZE / 48;
 
-    std::cout << "Blocksize: " << BLOCK_SIZE << "\n";
-    std::cout << "Processing took " << executionTime << " ms\n";
-    std::cout << "Input is " << inputTime << " ms long\n";
     std::cout << "Processor load is " << 100.0 * executionTime / inputTime << "%\n";
 
     return EXIT_SUCCESS;
