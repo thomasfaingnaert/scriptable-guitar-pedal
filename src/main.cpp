ddabdf78c87f09e49d2793b2d3f9746774911078
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-11 18:16:06 +0200

    Add timing code for FFT Convolver

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,28 @@ float timeDirectForm(const unsigned int impulseSize, const unsigned int blockSiz
     return 100.0 * executionTime / inputTime;
 }
 
+float timeFFT(const unsigned int impulseSize, const unsigned int blockSize, const unsigned int numBlocks)
+{
+    std::vector<float> impulseResponse(impulseSize);
+    std::generate(impulseResponse.begin(), impulseResponse.end(), std::rand);
+
+    FFTConvolver fftConvolver(impulseResponse, blockSize);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (unsigned int i = 0; i < numBlocks; ++i)
+    {
+        std::vector<float> input(blockSize);
+        std::generate(input.begin(), input.end(), std::rand);
+        std::vector<float> result = fftConvolver.process(input);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    const auto executionTime = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    const unsigned long long inputTime = numBlocks * blockSize / 48;
+
+    // calculate processor load
+    return 100.0 * executionTime / inputTime;
+}
+
 void testFFTConv()
 {
     unsigned int blockSize = 16;
@@ -70,9 +92,9 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    testFFTConv();
+    //testFFTConv();
 
-#if 0
+#if 1
     constexpr unsigned int FIELD_WIDTH = 30;
     constexpr unsigned int NUM_TRIALS = 10;
 
@@ -83,7 +105,7 @@ int main(int argc, char *argv[])
         unsigned int impulseSize = 48000 * impulseDuration;
 
         std::cout << std::left;
-        std::cout << std::setw(FIELD_WIDTH) << "Type: " << "Direct Form (Neon)" << "\n";
+        std::cout << std::setw(FIELD_WIDTH) << "Type: " << "FFT (Neon)" << "\n";
         std::cout << std::setw(FIELD_WIDTH) << "Impulse length: " << (float)impulseSize / 48 << " ms\n";
         std::cout << std::setw(FIELD_WIDTH) << "#Trials per blocksize: " << NUM_TRIALS << "\n";
         std::cout << std::setw(FIELD_WIDTH) << "Blocksize (samples)" << std::setw(FIELD_WIDTH) << "Load (%)" << "\n";
@@ -95,7 +117,7 @@ int main(int argc, char *argv[])
 
             for (unsigned int i = 0; i < NUM_TRIALS; ++i)
             {
-                load += timeDirectForm(impulseSize, blockSize, numBlocks);
+                load += timeFFT(impulseSize, blockSize, numBlocks);
             }
 
             std::cout << std::setw(FIELD_WIDTH) << blockSize << std::setw(FIELD_WIDTH) << load / NUM_TRIALS << std::endl;
