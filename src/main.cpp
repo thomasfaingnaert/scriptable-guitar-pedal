b8c42e34f6745143ef983b995ce61a8b36d62eee
Caro Meysmans <caro.meysmans@gmail.com>
2018-04-14 19:35:32 +0200

    Fix build error

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,6 @@
 #include <iostream>
 
 #include "NE10.h"
-#include "adder.h"
-#include "alsadevice.h"
-#include "blockbuffer.h"
-#include "civetweb.h"
-#include "codec.h"
-#include "convolver.h"
-#include "delayeffect.h"
-#include "distortioneffect.h"
-#include "filesink.h"
-#include "filesource.h"
-#include "luaeffect.h"
-#include "processor.h"
-#include "sinesource.h"
-#include "sink.h"
-#include "source.h"
-#include "streamsink.h"
-#include "tremoloeffect.h"
-#include "webserver.h"
 
 int main(int argc, char *argv[])
 {
