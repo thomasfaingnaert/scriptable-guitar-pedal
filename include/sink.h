01ab45f4dc80daf72e09912173b80ebe7ed05db4
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-19 20:10:26 +0200

    Add new version Source and Sink

--- include/sink.h
+++ include/sink.h
@@ -1,17 +1,14 @@
-#include <memory>
+#ifndef SINK_H_D2GUWTG5
+#define SINK_H_D2GUWTG5
+
 #include <vector>
-
-#include <cstddef>
-
-#ifndef SINK_H_FUKZZ1SW
-#define SINK_H_FUKZZ1SW
 
 template<typename T>
 class Sink
 {
     public:
-        virtual ~Sink() { };
-        virtual void push(const std::shared_ptr<std::vector<T>>& t, unsigned int channel) = 0;
+        virtual ~Sink() { }
+        virtual void push(const std::vector<T>& data) = 0;
 };
 
-#endif /* end of include guard: SINK_H_FUKZZ1SW */
+#endif /* end of include guard: SINK_H_D2GUWTG5 */
--- include/source.h
+++ include/source.h
@@ -1,51 +1,31 @@
-#include <cstddef>
+#ifndef SOURCE_H_TFQHODIC
+#define SOURCE_H_TFQHODIC
+
 #include <memory>
-#include <stdexcept>
 #include <vector>
 
-#include "sink.h"
-
-#ifndef SOURCE_H_WUKQSVNX
-#define SOURCE_H_WUKQSVNX
+template<typename T>
+class Sink;
 
 template<typename T>
 class Source
 {
     public:
-        struct Connection
+        void connect(const std::shared_ptr<Sink>& sink)
         {
-            std::shared_ptr<Sink<T>> sink;
-            unsigned int channel;
-        };
-
-        void connect(const std::shared_ptr<Sink<T>>& sink, unsigned int channel)
-        {
-            Connection c;
-            c.sink = sink;
-            c.channel = channel;
-            connections.push_back(c);
+            connectedSinks.push_back(sink);
         }
 
-        void generate(const std::shared_ptr<std::vector<T>>& t)
+        void generate(const std::vector<T>& data)
         {
-            if (t->size() != BLOCK_SIZE)
-                throw std::invalid_argument("Samples should be BLOCK_SIZE long");
-
-            // update all connected sinks
-            for (Connection& c : connections)
+            for (const std::shared_ptr<Sink<T>>& sink : connectedSinks)
             {
-                c.sink->push(t, c.channel);
+                sink->push(data);
             }
         }
 
-        static constexpr std::size_t BLOCK_SIZE = 256;
-
     private:
-        std::vector<Connection> connections;
+        std::vector<std::shared_ptr<Sink<T>>> connectedSinks;
 };
 
-template<typename T>
-constexpr std::size_t Source<T>::BLOCK_SIZE;
-
-
-#endif /* end of include guard: SOURCE_H_WUKQSVNX */
+#endif /* end of include guard: SOURCE_H_TFQHODIC */
