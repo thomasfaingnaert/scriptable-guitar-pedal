7b9bc8959cba91fa758ea4816ce4efe89c57c9b7
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-21 10:27:10 +0200

    Add dl library to FindLua.cmake

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -1,2 +1,3 @@
 find_path(LUA_INCLUDE_DIRS lua.h PATHS "${CMAKE_SOURCE_DIR}/include/lua")
-find_library(LUA_LIBRARIES liblua.a PATHS "${CMAKE_SOURCE_DIR}/lib")
+find_library(LUA_LIBRARY liblua.a PATHS "${CMAKE_SOURCE_DIR}/lib")
+set(LUA_LIBRARIES ${LUA_LIBRARY} dl)
