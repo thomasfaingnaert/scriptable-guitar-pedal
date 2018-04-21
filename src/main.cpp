5be480e5e6d1ae45f3c6239a0568ab95537f1db0
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-21 16:51:48 +0200

    Implement basic PRU timer

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
+#include <cstring>
 #include <cstdlib>
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
 #include <iostream>
+#include <pthread.h>
+#include <thread>
+#include <chrono>
+
+void *pruevtout0_thread(void *arg)
+{
+    static auto start = std::chrono::high_resolution_clock::now();
+
+    sched_param params;
+    params.sched_priority = 99;
+    if (pthread_setschedparam(pthread_self(), SCHED_RR, &params))
+    {
+        std::cerr << "Failed to set priority for thread: " << std::strerror(errno) << "\n";
+    }
+
+    while (1)
+    {
+        prussdrv_pru_wait_event(PRU_EVTOUT_0);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "took " << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() << " us\n";
+        start = end;
+        prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +48,13 @@ int main(int argc, char *argv[])
 
     prussdrv_pruintc_init(&pruss_intc_initdata);
 
+    pthread_t irq_thread;
+    if (pthread_create(&irq_thread, nullptr, pruevtout0_thread, nullptr))
+    {
+        std::cerr << "Error creating IRQ thread" << std::endl;
+        return EXIT_FAILURE;
+    }
+
     std::cout << "Running program and waiting for termination..." << std::endl;
 
     if (prussdrv_exec_program(PRU0, argv[1]) < 0)
@@ -33,6 +65,7 @@ int main(int argc, char *argv[])
 
     prussdrv_pru_wait_event(PRU_EVTOUT_0);
 
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     std::cout << "Done" << std::endl;
 
     prussdrv_pru_disable(PRU0);
