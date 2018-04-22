6b23341ea8bb046d2be0adfc353b5f4b5748c050
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-22 23:32:06 +0200

    Add Xenomai userspace libraries (crosscompile)

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,15 @@
-#include <iostream>
+#include <pthread.h>
+#include <stdio.h>
 
-#include "pru.h"
-
-int main(int argc, char *argv[])
+void* func(void* arg)
 {
-    if (argc != 2)
-    {
-        std::cout << "Usage: " << argv[0] << " filename.bin" << std::endl;
-        return EXIT_SUCCESS;
-    }
+    while (1) ;
+}
 
-    PRU pru;
-    ulong* sharedMemory = pru.setupSharedMemory();
-
-    pru.executeProgram(argv[1]);
-
-    while (true)
-    {
-        // First read input
-        int input;
-        std::cout << "enter number: ";
-        std::cin >> input;
-
-        // sharedMemory[0] = ready? and sharedMemory[1] = data
-        sharedMemory[1] = input;
-        sharedMemory[0] = 1;
-
-        std::cout << "Waiting for the PRU to write response..." << std::endl;
-
-        // wait for interrupt
-        pru.waitForInterrupt();
-
-        std::cout << "PRU gave result: " << sharedMemory[1] << std::endl;
-    }
-
-    return EXIT_SUCCESS;
+int main()
+{
+    pthread_t thread;
+    pthread_create(&thread, NULL, func, NULL);
+    pthread_join(thread, NULL);
 }
 
