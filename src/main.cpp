4f4584de591a5f27611a93f886d3dffbe399ff72
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-22 10:28:28 +0200

    Change PRU example to use PRU class

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,6 @@
-#include <cstring>
-#include <cstdlib>
-#include <prussdrv.h>
-#include <pruss_intc_mapping.h>
 #include <iostream>
-#include <pthread.h>
-#include <thread>
-#include <chrono>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <unistd.h>
 
-#define PRU_ADDR 0x4a300000
-#define SHAREDRAM_OFFSET 0x10000
+#include "pru.h"
 
 int main(int argc, char *argv[])
 {
@@ -21,30 +10,10 @@ int main(int argc, char *argv[])
         return EXIT_SUCCESS;
     }
 
-    int fd = open("/dev/mem", O_RDWR | O_SYNC);
-    ulong* sharedmem = (ulong*) mmap(0, 0x10000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, PRU_ADDR + SHAREDRAM_OFFSET);
-    // Initialise ACK to 0
-    sharedmem[0] = 0;
+    PRU pru;
+    ulong* sharedMemory = pru.setupSharedMemory();
 
-    prussdrv_init();
-
-    if (prussdrv_open(PRU_EVTOUT_0) == -1)
-    {
-        std::cerr << "prussdrv_open() failed" << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
-
-    prussdrv_pruintc_init(&pruss_intc_initdata);
-
-    std::cout << "Running program and waiting for termination..." << std::endl;
-
-    if (prussdrv_exec_program(PRU0, argv[1]) < 0)
-    {
-        std::cerr << "Error loading " << argv[1] << std::endl;
-        return EXIT_FAILURE;
-    }
+    pru.executeProgram(argv[1]);
 
     while (true)
     {
@@ -53,22 +22,17 @@ int main(int argc, char *argv[])
         std::cout << "enter number: ";
         std::cin >> input;
 
-        // sharedmem[0] = ready? and sharedmem[1] = data
-        sharedmem[1] = input;
-        sharedmem[0] = 1;
+        // sharedMemory[0] = ready? and sharedMemory[1] = data
+        sharedMemory[1] = input;
+        sharedMemory[0] = 1;
 
         std::cout << "Waiting for the PRU to write response..." << std::endl;
 
         // wait for interrupt
-        prussdrv_pru_wait_event(PRU_EVTOUT_0);
-        prussdrv_pru_clear_event(PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+        pru.waitForInterrupt();
 
-        std::cout << "PRU gave result: " << sharedmem[1] << std::endl;
+        std::cout << "PRU gave result: " << sharedMemory[1] << std::endl;
     }
-
-    std::cout << "Done" << std::endl;
-    prussdrv_pru_disable(PRU0);
-    prussdrv_exit();
 
     return EXIT_SUCCESS;
 }
