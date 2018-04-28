7ebbcded806ebbfc1835c32ca235f16271045a20
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-28 20:04:34 +0200

    Implement fdl performance test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <algorithm>
+#include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
-#include <iomanip>
+#include <vector>
 
 #include "frequencydelayline.h"
+#include "sampledata.h"
 
 int main(int argc, char *argv[])
 {
@@ -14,19 +17,63 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    static constexpr unsigned int BLOCK_SIZE = 16;
+    constexpr unsigned int BLOCK_SIZE = 16384;
 
+    std::cout << "Block size: " << BLOCK_SIZE << std::endl;
 
-    std::array<float, BLOCK_SIZE+1> impulse1{0,1};
-    std::array<float, BLOCK_SIZE+1> impulse2{};
-    FrequencyDelayLine<BLOCK_SIZE> fdl({impulse1,impulse2});
+    std::cout << "Loading wavs..." << std::endl;
+    SampleData input("input.wav");
+    SampleData impulse("impulse.wav");
 
-    std::array<float, BLOCK_SIZE> input{1,2,3,4};
-    auto res = fdl.process(input);
+    auto inputSamples = input.getSamples()[0];
+    auto impulseSamples = impulse.getSamples()[0];
 
-    std::cout << std::setprecision(3);
-    std::copy(res.begin(), res.end(), std::ostream_iterator<float>(std::cout, " "));
-    std::cout << "\n";
+    std::cout << "Partitioning impulse..." << std::endl;
+    std::vector<std::array<float,BLOCK_SIZE+1>> impulseResponses;
+
+    auto it = impulseSamples.begin();
+
+    while (it != impulseSamples.end())
+    {
+        impulseResponses.emplace_back();
+
+        if (std::distance(it, impulseSamples.end()) < BLOCK_SIZE+1)
+        {
+            std::copy(it, impulseSamples.end(), impulseResponses.back().begin());
+            it = impulseSamples.end();
+        }
+        else
+        {
+            std::copy_n(it, BLOCK_SIZE+1, impulseResponses.back().begin());
+            it += BLOCK_SIZE + 1;
+        }
+    }
+
+    std::cout << "Creating fdl..." << std::endl;
+    FrequencyDelayLine<BLOCK_SIZE> fdl(impulseResponses);
+
+    auto inIt = inputSamples.begin();
+    std::vector<float> outputSamples;
+
+    std::cout << "Processing..." << std::endl;
+    auto begin = std::chrono::high_resolution_clock::now();
+    while (inIt + BLOCK_SIZE < inputSamples.end())
+    {
+        std::array<float, BLOCK_SIZE> block;
+        std::copy_n(inIt, BLOCK_SIZE, block.begin());
+
+        auto res = fdl.process(block);
+        outputSamples.insert(outputSamples.end(), res.begin(), res.end());
+        inIt += BLOCK_SIZE;
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Processing took " << std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count() << " ms" << std::endl;
+
+    std::cout << "Saving..." << std::endl;
+    SampleData out({outputSamples}, input.getSampleRate());
+    out.save("output.wav");
+    std::cout << "Saved" << std::endl;
 
     return EXIT_SUCCESS;
 }
