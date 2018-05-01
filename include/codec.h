5cca07d15747868be4ab15f561181fde8f52cfbe
Caro Meysmans <caro.meysmans@gmail.com>
2018-05-01 17:40:34 +0200

    Add Mode Control 1 write operations

--- include/codec.h
+++ include/codec.h
@@ -6,22 +6,70 @@
 class Codec : I2CDevice
 {
 public:
-    Codec(unsigned int adapter, uint16_t deviceAddress);
+    Codec(unsigned int adapter, std::uint16_t deviceAddress);
 
-    uint8_t getModeControl1();
-    uint8_t getModeControl2();
+    enum FunctionalMode : std::uint8_t
+    {
+        SINGLE_SPEED_MODE_1 = 0b00,
+        SINGLE_SPEED_MODE_2 = 0b01,
+        DOUBLE_SPEED_MODE = 0b10,
+        QUAD_SPEED_MODE = 0b11
+    };
 
-    uint8_t getDACControl();
-    uint8_t getDACVolumeAndMixingControl();
-    uint8_t getDACChannelAVolumeControl();
-    uint8_t getDACChannelBVolumeControl();
+    static constexpr std::uint8_t functionalModeOffset = 6;
 
-    uint8_t getADCControl();
+    void setFunctionalMode(FunctionalMode functionalMode)
+    {
+        writeRegister(MODE_CONTROL_1, functionalMode << functionalModeOffset);
+    };
 
-    uint8_t getChipID();
+    enum ClockingRatio : std::uint8_t
+    {
+        CLOCKING_RATIO_1 = 0b00,
+        CLOCKING_RATIO_2 = 0b01,
+        CLOCKING_RATIO_3 = 0b10,
+        CLOCKING_RATIO_4 = 0b11
+    };
 
-protected:
-    enum Register : uint8_t
+    static constexpr std::uint8_t clockingRatioOffset = 4;
+
+    void setClockingRatio(ClockingRatio clockingRatio)
+    {
+        writeRegister(MODE_CONTROL_1, clockingRatio << clockingRatioOffset);
+    }
+
+    enum MasterSlaveMode : std::uint8_t
+    {
+        SLAVE = 0b0,
+        MASTER = 0b1
+    };
+
+    static constexpr std::uint8_t masterSlaveModeOffset = 3;
+
+    void setMasterSlaveMode(MasterSlaveMode masterSlaveMode)
+    {
+        writeRegister(MODE_CONTROL_1, masterSlaveMode << masterSlaveModeOffset);
+    }
+
+    enum DigitalInterfaceFormat : std::uint8_t
+    {
+        LEFT_JUSTIFIED = 0b000,
+        INTER_IC_SOUND = 0b001,
+        RIGHT_JUSTIFIED_16 = 0b010,
+        RIGHT_JUSTIFIED_18 = 0b101,
+        RIGHT_JUSTIFIED_20 = 0b100,
+        RIGHT_JUSTIFIED_24 = 0b011
+    };
+
+    static constexpr std::uint8_t digitalInterfaceFormatOffset = 0;
+
+    void setDACDigitalInterfaceFormat(DigitalInterfaceFormat digitalInterfaceFormat)
+    {
+        writeRegister(MODE_CONTROL_1, digitalInterfaceFormat << digitalInterfaceFormatOffset);
+    }
+
+private:
+    enum Register : std::uint8_t
     {
         MODE_CONTROL_1 = 0x01,
         DAC_CONTROL = 0x02,
--- src/codec.cpp
+++ src/codec.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <thread>
+
 #include "codec.h"
 
 Codec::Codec(unsigned int adapter, uint16_t deviceAddress) : I2CDevice(adapter, deviceAddress)
@@ -5,42 +9,11 @@ Codec::Codec(unsigned int adapter, uint16_t deviceAddress) : I2CDevice(adapter, 
 
 }
 
-uint8_t Codec::getModeControl1()
+void Codec::setFunctionalMode(Codec::FunctionalMode functionalMode)
 {
-    return readRegister(MODE_CONTROL_1);
 }
 
-uint8_t Codec::getModeControl2()
+Codec::FunctionalMode Codec::getFunctionalMode() const
 {
-    return readRegister(MODE_CONTROL_2);
-}
 
-uint8_t Codec::getDACControl()
-{
-    return readRegister(DAC_CONTROL);
-}
-
-uint8_t Codec::getDACVolumeAndMixingControl()
-{
-    return readRegister(DAC_VOLUME_AND_MIXING_CONTROL);
-}
-
-uint8_t Codec::getDACChannelAVolumeControl()
-{
-    return readRegister(DAC_CHANNEL_A_VOLUME_CONTROL);
-}
-
-uint8_t Codec::getDACChannelBVolumeControl()
-{
-    return readRegister(DAC_CHANNEL_B_VOLUME_CONTROL);
-}
-
-uint8_t Codec::getADCControl()
-{
-    return readRegister(ADC_CONTROL);
-}
-
-uint8_t Codec::getChipID()
-{
-    return readRegister(CHIP_ID);
 }
