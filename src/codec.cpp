65936f4b5c5671b7f409e5e35b015a3d62c36b30
Caro Meysmans <caro.meysmans@gmail.com>
2018-05-07 16:14:31 +0200

    Add the necessary delays to the codec initialization procedure

--- src/codec.cpp
+++ src/codec.cpp
@@ -14,14 +14,17 @@ Codec::Codec(unsigned int adapter, uint16_t deviceAddress) : I2CDevice(adapter, 
     gpio50_direction << "low";
     gpio50_direction.close();
 
-    // Give the codec time to reset
+    // Reset the codec
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
     std::ofstream gpio50_value("/sys/class/gpio/gpio50/value");
     gpio50_value << 1;
     gpio50_value.close();
 
-    // Enable control port mode and power down the device
+    // MCLK will appear prior to 1 ms from the release of not reset
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+    // Activate the control port and place the part in power-down
     writeRegister(MODE_CONTROL_2, 0b00000011);
 
     // Select master mode
@@ -29,4 +32,7 @@ Codec::Codec(unsigned int adapter, uint16_t deviceAddress) : I2CDevice(adapter, 
 
     // Power up the device
     writeRegister(MODE_CONTROL_2, 0b00000010);
+
+    // The power-up sequence requires approximately 85 us
+    std::this_thread::sleep_for(std::chrono::microseconds(85));
 }
